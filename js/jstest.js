b82e50f1a5e78f74268f848f7138afc5953dc89b
jaewoo-926 <heg14242@gmail.com>
2025-04-02 14:19:36 +0900

    5주차 테스트

---
+++ js/jstest.js
@@ -0,0 +1,25 @@
+var jb = 'hi'; //변수 선언 후 주석 가능 한줄 주석
+var a = 1;
+var b;
+b = 5;
+
+/*
+여러줄 주석 처리
+*/
+
+if(true){
+    let C = 'let 접근';
+    var c_1 = 'var 접근';
+}
+//console.log(c); // Error?
+console.log(c_1);
+
+let d = 5;
+//let d = '값 재할당'; // Error?
+console.log(d);
+
+const e = '상수1 접근';
+//e
+//const f //Error?
+console.log(e);
+
