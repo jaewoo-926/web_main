1ccec8c830a7b1087c729b3027f6440a470b8081
jaewoo-926 <heg14242@gmail.com>
2025-04-16 23:29:21 +0900

    4주차 강의자료 응용문제, 연습문제 풀기

---
+++ HomeWork_js/HomeWork_js_search.js
@@ -0,0 +1,16 @@
+document.getElementById("search_button_msg ").addEventListener('click', search_message);
+function search_message(){
+    let message = "검색을 수행합니다!";
+    alert(message);
+ }
+
+// function search_message(){
+// alert("검색을 수행합니다!");
+// }
+
+// function search_message(){
+// alert("검색을 수행합니다!");
+// }  밑에 있는 함수에 우선순위가 생김
+
+
+
