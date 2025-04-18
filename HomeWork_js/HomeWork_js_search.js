90dc9f716139274b3f9b5e1d1db80a61bd8cadaa
jaewoo-926 <heg14242@gmail.com>
2025-04-18 21:38:00 +0900

    5주차 강의자료 응용문제, 연습문제 풀기

--- HomeWork_js/HomeWork_js_search.js
+++ HomeWork_js/HomeWork_js_search.js
@@ -2,8 +2,28 @@ document.getElementById("search_button_msg ").addEventListener('click', search_m
 function search_message(){
     let message = "검색을 수행합니다!";
     alert(message);
- }
+  }
+ function googleSearch() {
+    const badWords = ['바보', '멍청이', 'ㅅㅂ', 'ㅄ', '꺼져'];         // 검사할 비속어 리스트 (5개)
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    
+    if (searchTerm.length > 0) {  // 검색어가 0보다 크면 실행
+        // 비속어 배열을 반복하면서 하나씩 확인
+        for (let i = 0; i < badWords.length; i++) {
+          // 현재 비속어가 입력 문자열에 포함되어 있는지 확인
+          if (searchTerm.includes(badWords[i])) {
+            
+            return; // 비속어가 있으면 함수 중단
+          }
+        }
+          const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`; // 새 창에서 구글 검색을 수행
+          window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+        return false;
+      }
+    
 
+   
+    }
 // function search_message(){
 // alert("검색을 수행합니다!");
 // }
